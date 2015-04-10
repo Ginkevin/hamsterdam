55f19374e853768e97583104a20e600bb77d205f
emilnygaardfrisk <emilnygaardfrisk@gmail.com>
2015-04-10 11:29:02 +0200

    ..

---
+++ Core/src/main/java/dk/sdu/group3/semprojekt/core/Hamsterdam.java
@@ -0,0 +1,25 @@
+package dk.sdu.group3.semprojekt.core;
+
+import playn.core.Game;
+/**
+ * @author emilfrisk
+ */
+public class Hamsterdam extends Game.Default{
+
+	private Hamsterdam() {
+		super(33);
+	}
+
+	@Override
+	public void init() {
+	}
+
+	@Override
+	public void paint(float alpha) {
+	}
+
+	@Override
+	public void update(int delta) {
+		super.update(delta); //To change body of generated methods, choose Tools | Templates.
+	}
+}
