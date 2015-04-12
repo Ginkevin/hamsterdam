f8fcdbf734660bfec48a36949cc8cfd2e93659bb
emilnygaardfrisk <emilnygaardfrisk@gmail.com>
2015-04-12 16:50:07 +0200

    ...

--- Core/src/main/java/dk/sdu/group3/semprojekt/core/Hamsterdam.java
+++ Core/src/main/java/dk/sdu/group3/semprojekt/core/Hamsterdam.java
@@ -1,17 +1,27 @@
 package dk.sdu.group3.semprojekt.core;
 
+import dk.sdu.group3.semprojekt.common.IGamePlugin;
+import java.util.ArrayList;
+import java.util.List;
+import org.openide.util.Lookup;
 import playn.core.Game;
 /**
  * @author emilfrisk
  */
 public class Hamsterdam extends Game.Default{
 
+	List<IGamePlugin> plugins;
+	
 	private Hamsterdam() {
 		super(33);
 	}
 
 	@Override
 	public void init() {
+		Lookup.Result<IGamePlugin> result = Lookup.getDefault().lookupResult(IGamePlugin.class);
+		plugins = new ArrayList<IGamePlugin>(result.allInstances());
+
+			
 	}
 
 	@Override
