a0a5465507300d70a096da592f209e0514e25264
emilnygaardfrisk <emilnygaardfrisk@gmail.com>
2015-04-16 15:23:40 +0200

    Core works!

--- Common/src/main/java/dk/sdu/group3/semprojekt/common/data/Entity.java
+++ Common/src/main/java/dk/sdu/group3/semprojekt/common/data/Entity.java
@@ -3,77 +3,102 @@ package dk.sdu.group3.semprojekt.common.data;
 import dk.sdu.group3.semprojekt.common.interfaces.IEntity;
 import java.util.ArrayList;
 import java.util.List;
+import playn.core.ImageLayer;
 
 public abstract class Entity implements IEntity {
-    private Vector position;
-    private Vector velocity;
-    private String sprite;
-    private float angle;
-    private ShapeEnum shape;
-    private List<Event> events = new ArrayList();
-    
-    @Override
-    public Vector getPosition(){
-        return position;
-    }
-    
-    @Override
-    public void setPosition(float x, float y){
-        position.setVector(x, y);
-    }
-    
-    @Override
-    public Vector getVelocity(){
-        return velocity;
-    }
-    
-    @Override
-    public void setVelocity(float x, float y){
-        velocity.setVector(x, y);
-    }
-    
-    @Override
-    public String getSprite(){
-        return sprite;
-    }
-    
-    @Override
-    public void setSprite(String path){
-        sprite = path;
-    }
-    
-    @Override
-    public ShapeEnum getShape(){
-        return shape;
-    }
-    
-    @Override
-    public void setShape(ShapeEnum shape){
-        this.shape = shape;
-    }
-    
-    @Override
-    public void addEvent(Event e){
-        events.add(e);
-    }
-    
-    @Override
-    public List<Event> getEvents(){
-        return events;
-    }
-    
-    @Override
-    public void removeEvent(Event e){
-        events.remove(e);
-    }
-    
-    @Override
-    public float getAngle(){
-        return angle;
-    }
-    
-    @Override
-    public void setAngle(float angle){
-        this.angle = angle;
-    }
+
+	private Vector position;
+	private Vector velocity;
+	private String sprite;
+	private float angle;
+	private ShapeEnum shape;
+	private List<Event> events = new ArrayList();
+	private ImageLayer view;
+	private float scale;
+
+	@Override
+	public Vector getPosition() {
+		return position;
+	}
+
+	@Override
+	public void setPosition(float x, float y) {
+		position.setVector(x, y);
+	}
+
+	@Override
+	public Vector getVelocity() {
+		return velocity;
+	}
+
+	@Override
+	public void setVelocity(float x, float y) {
+		velocity.setVector(x, y);
+	}
+
+	@Override
+	public String getSprite() {
+		return sprite;
+	}
+
+	@Override
+	public void setSprite(String path) {
+		sprite = path;
+	}
+
+	@Override
+	public ShapeEnum getShape() {
+		return shape;
+	}
+
+	@Override
+	public void setShape(ShapeEnum shape) {
+		this.shape = shape;
+	}
+
+	@Override
+	public void addEvent(Event e) {
+		events.add(e);
+	}
+
+	@Override
+	public List<Event> getEvents() {
+		return events;
+	}
+
+	@Override
+	public void removeEvent(Event e) {
+		events.remove(e);
+	}
+
+	@Override
+	public float getAngle() {
+		return angle;
+	}
+
+	@Override
+	public void setAngle(float angle) {
+		this.angle = angle;
+	}
+
+	@Override
+	public ImageLayer getView() {
+		return view;
+	}
+
+	@Override
+	public void setView(ImageLayer view) {
+		this.view = view;
+	}
+
+	@Override
+	public float getScale() {
+		return scale;
+	}
+
+	@Override
+	public void setScale(float scale) {
+		this.scale = scale;
+	}
+	
 }
--- Core/src/main/java/dk/sdu/group3/semprojekt/core/Installer.java
+++ Core/src/main/java/dk/sdu/group3/semprojekt/core/Installer.java
@@ -5,16 +5,25 @@
  */
 package dk.sdu.group3.semprojekt.core;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import org.openide.modules.ModuleInstall;
 import playn.core.PlayN;
 import playn.java.JavaPlatform;
 
 public class Installer extends ModuleInstall {
 
+	ScheduledExecutorService e = Executors.newScheduledThreadPool(1);
+
 	@Override
 	public void restored() {
 		JavaPlatform.Config config = new JavaPlatform.Config();
 		JavaPlatform.register(config);
+
+		e.schedule(()->{
+			PlayN.run(new Hamsterdam());
+		}, 1000, TimeUnit.MILLISECONDS);
 		PlayN.run(new Hamsterdam());
 	}
 
