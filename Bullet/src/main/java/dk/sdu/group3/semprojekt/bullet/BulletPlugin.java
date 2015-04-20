4ce936d03089a7e0fb855546eb44047645d5e1a0
Henrik Frank <frank@disko-fd.dk>
2015-04-20 22:32:34 +0200

    Bullet halløj
    Conflicts:
    	player/target/classes/META-INF/MANIFEST.MF
    	player/target/maven-archiver/pom.properties
    
    player/target/nbm/netbeans/hamsterdam/modules/dk-sdu-group3-semprojekt-p
    layer.jar
    
    player/target/nbm/netbeans/hamsterdam/update_tracking/dk-sdu-group3-semp
    rojekt-player.xml
    	player/target/nbm/player-0.0.1-SNAPSHOT.nbm
    	player/target/player-0.0.1-SNAPSHOT.jar
    	player/target/player-0.0.1-SNAPSHOT.nbm
    	pom.xml

---
+++ Bullet/src/main/java/dk/sdu/group3/semprojekt/bullet/BulletPlugin.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.group3.semprojekt.bullet;
+
+import dk.sdu.group3.semprojekt.common.data.Bullet;
+import dk.sdu.group3.semprojekt.common.data.World;
+import dk.sdu.group3.semprojekt.common.spi.IGamePlugin;
+
+/**
+ *
+ * @author henrikfrank
+ */
+public class BulletPlugin implements IGamePlugin{
+
+    @Override
+    public void start(World world) {
+
+    }
+
+    @Override
+    public void stop(World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ Bullet/src/main/java/dk/sdu/group3/semprojekt/bullet/BulletProcess.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.group3.semprojekt.bullet;
+
+import dk.sdu.group3.semprojekt.common.interfaces.IEntity;
+import dk.sdu.group3.semprojekt.common.spi.IGameProcess;
+import java.util.List;
+
+/**
+ *
+ * @author henrikfrank
+ */
+public class BulletProcess implements IGameProcess{
+
+    @Override
+    public void process(int delta, List<IEntity> entities) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
