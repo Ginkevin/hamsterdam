06def219c9163116f721e522196e61de7779c162
emilnygaardfrisk <emilnygaardfrisk@gmail.com>
2015-04-22 00:20:38 +0200

    new branch

---
+++ Weapon/src/main/java/dk/sdu/group3/semprojekt/weapon/WeaponProcess.java
@@ -0,0 +1,18 @@
+package dk.sdu.group3.semprojekt.weapon;
+
+import dk.sdu.group3.semprojekt.common.data.World;
+import dk.sdu.group3.semprojekt.common.interfaces.IEntity;
+import dk.sdu.group3.semprojekt.common.spi.IGameProcess;
+import java.util.List;
+
+/**
+ * @author emilfrisk
+ */
+public class WeaponProcess  implements IGameProcess {
+
+	@Override
+	public void process(int delta, World world) {
+		// if event fire is in eventlist
+			// make weapon fire
+	}
+}
