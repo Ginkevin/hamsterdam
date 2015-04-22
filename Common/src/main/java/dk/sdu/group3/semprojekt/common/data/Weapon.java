ef5fb91d9ccc47016058adcbfd61f2c1b626e22f
emilnygaardfrisk <emilnygaardfrisk@gmail.com>
2015-04-22 10:55:59 +0200

    weapon can shoot

--- Common/src/main/java/dk/sdu/group3/semprojekt/common/data/Weapon.java
+++ Common/src/main/java/dk/sdu/group3/semprojekt/common/data/Weapon.java
@@ -1,40 +1,42 @@
 package dk.sdu.group3.semprojekt.common.data;
 
 public class Weapon extends Entity {
-    private BulletFactory bulletFactory;
-    private int ammo;
-    private int range;
-    private int cooldown;
-    
-    public BulletFactory getBulletFactory(){
-        return bulletFactory;
-    }
-    
-    public void setBulletFactory(BulletFactory b){
-        bulletFactory = b;
-    }
-    
-    public int getAmmo(){
-        return ammo;
-    }
-    
-    public void setAmmo(int ammo){
-        this.ammo = ammo;
-    }
-    
-    public int getRange(){
-        return range;
-    }
-    
-    public void setRange(int range){
-        this.range = range;
-    }
-    
-    public int getCooldown(){
-        return cooldown;
-    }
-    
-    public void setCooldown(int cd){
-        cooldown = cd;
-    }
+
+	private BulletFactory bulletFactory;
+	private int ammo;
+	private int range;
+	private int cooldown;
+
+	public BulletFactory getBulletFactory() {
+		return bulletFactory;
+	}
+
+	public void setBulletFactory(BulletFactory b) {
+		bulletFactory = b;
+	}
+
+	public int getAmmo() {
+		return ammo;
+	}
+
+	public void setAmmo(int ammo) {
+		this.ammo = ammo;
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+
+	public int getCooldown() {
+		return cooldown;
+	}
+
+	public void setCooldown(int cd) {
+		cooldown = cd;
+	}
+
 }
--- Weapon/src/main/java/dk/sdu/group3/semprojekt/weapon/WeaponProcess.java
+++ Weapon/src/main/java/dk/sdu/group3/semprojekt/weapon/WeaponProcess.java
@@ -1,9 +1,9 @@
-package dk.sdu.group3.semprojekt.weapon;
-
+package dk.sdu.group3.semprojekt.weapon; 
+import dk.sdu.group3.semprojekt.common.data.Bullet;
+import dk.sdu.group3.semprojekt.common.data.EventEnum;
+import dk.sdu.group3.semprojekt.common.data.Weapon;
 import dk.sdu.group3.semprojekt.common.data.World;
-import dk.sdu.group3.semprojekt.common.interfaces.IEntity;
 import dk.sdu.group3.semprojekt.common.spi.IGameProcess;
-import java.util.List;
 
 /**
  * @author emilfrisk
@@ -12,7 +12,19 @@ public class WeaponProcess  implements IGameProcess {
 
 	@Override
 	public void process(int delta, World world) {
-		// if event fire is in eventlist
-			// make weapon fire
+		world.getEvents().stream().forEach((e)->{
+			if (e.getEvent() == EventEnum.SHOOT){
+				world.getEntities().forEach((w)->{
+					if (w instanceof Weapon){
+						// SHOOT
+						Weapon weapon = (Weapon) w;
+						Bullet b = weapon.getBulletFactory().createBullet();
+						b.setPosition(weapon.getPosition().getX()+10, weapon.getPosition().getY()+10);
+						b.setVelocity(10, 10);
+						b.setAngle(weapon.getAngle());
+					}
+				});
+			}
+		});
 	}
 }
