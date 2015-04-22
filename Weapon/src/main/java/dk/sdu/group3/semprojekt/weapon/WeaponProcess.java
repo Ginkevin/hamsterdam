df1f449e468156890d3ee8a6bd4d0b1fe4cebe0d
emilnygaardfrisk <emilnygaardfrisk@gmail.com>
2015-04-22 12:49:28 +0200

    fixed import error

--- Weapon/src/main/java/dk/sdu/group3/semprojekt/weapon/WeaponProcess.java
+++ Weapon/src/main/java/dk/sdu/group3/semprojekt/weapon/WeaponProcess.java
@@ -1,7 +1,4 @@
 package dk.sdu.group3.semprojekt.weapon; 
-import dk.sdu.group3.semprojekt.common.data.Event;
-import dk.sdu.group3.semprojekt.common.data.EventEnum;
-import static dk.sdu.group3.semprojekt.common.data.EventEnum.CREATEBULLET;
 import dk.sdu.group3.semprojekt.common.data.World;
 import dk.sdu.group3.semprojekt.common.spi.IGameProcess;
 
