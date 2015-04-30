8ea936d8bf9692cbe05db604da5fd5e12a4e5e3c
Emil Frisk <emilnygaardfrisk@gmail.com>
2015-04-30 14:24:00 +0200

    Implemented PlayerWeapon component

--- PlayerWeapon/src/main/java/dk/sdu/group3/semprojekt/playerweapon/Joint.java
+++ PlayerWeapon/src/main/java/dk/sdu/group3/semprojekt/playerweapon/Joint.java
@@ -5,10 +5,27 @@
  */
 package dk.sdu.group3.semprojekt.playerweapon;
 
+import dk.sdu.group3.semprojekt.common.data.Bullet;
+import dk.sdu.group3.semprojekt.common.data.ShapeEnum;
+
 /**
  *
  * @author E
  */
-public class Joint {
-	
+public class Joint extends Bullet {
+
+	private String url = this.getClass().getClassLoader().getResource("images/Hale.png").toExternalForm();
+
+	public Joint() {
+		//Entity
+		setPosition(100, 100);
+		setVelocity(0, 0);
+		setSprite(url);
+		setShape(ShapeEnum.SQUARE);
+		setAngle(0);
+		setRadius(10);
+
+		//Bullet
+		setDamage(1);
+	}
 }
