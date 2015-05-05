a1d2cbeee71cf2798ed9b2c345a5d67973ce7326
Emil Frisk <emilnygaardfrisk@gmail.com>
2015-05-05 14:44:34 +0200

    corrected playerweapon process

--- PlayerWeapon/src/main/java/dk/sdu/group3/semprojekt/playerweapon/PlayerWeaponProcess.java
+++ PlayerWeapon/src/main/java/dk/sdu/group3/semprojekt/playerweapon/PlayerWeaponProcess.java
@@ -7,6 +7,7 @@ package dk.sdu.group3.semprojekt.playerweapon;
 
 import dk.sdu.group3.semprojekt.common.data.Event;
 import static dk.sdu.group3.semprojekt.common.data.EventEnum.SHOOT;
+import static dk.sdu.group3.semprojekt.common.data.EventEnum.SPACE;
 import dk.sdu.group3.semprojekt.common.data.World;
 import dk.sdu.group3.semprojekt.common.interfaces.ICharacter;
 import dk.sdu.group3.semprojekt.common.interfaces.IEntity;
@@ -22,6 +23,24 @@ public class PlayerWeaponProcess implements IGameProcess {
 		for (IEntity e : world.getEntities()) {
 			if (e instanceof ICharacter) {
 				if (((ICharacter) e).getWeapon() instanceof PlayerWeapon) {
+					
+					/***
+					 * Right now the Move module adds shoot events to world, which this block supports
+					 */
+					for (Event ev : world.getEvents()){
+						if (ev.getEvent() == SPACE){
+							Joint j = new Joint();
+							j.setPosition(e.getPosition().getX(), e.getPosition().getY());
+							j.setAngle(e.getAngle());
+							j.setVelocity(10,10);
+							world.addEntity(j);
+							world.removeEvent(ev);
+						}
+					}
+					/***
+					 * This block is how shoot is supposed to be implemented. Doesn't work right now because of Move.
+					 * TODO: change Move module to add shoot events to player instead of world, which will make this block valid
+					 */
 					for (Event ev : e.getEvents()) {
 						if (ev.getEvent() == SHOOT) {
 							Joint j = new Joint();
