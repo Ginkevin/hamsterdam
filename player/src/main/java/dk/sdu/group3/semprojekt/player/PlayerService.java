3cc771b6f640906c5d41e0db71d06a7efd5b3c03
emilnygaardfrisk <emilnygaardfrisk@gmail.com>
2015-05-14 23:30:13 +0200

    small delete

--- player/src/main/java/dk/sdu/group3/semprojekt/player/PlayerService.java
+++ player/src/main/java/dk/sdu/group3/semprojekt/player/PlayerService.java
@@ -33,83 +33,80 @@ import org.openide.util.lookup.ServiceProvider;
  */
 @ServiceProvider(service = IGameProcess.class)
 public class PlayerService implements IGameProcess {
-	int jumpCooldown = 1600, currentJumpCooldown = 1600;
+
+	int jumpCooldown = 1600, currentJumpCooldown = 0;
 	boolean jump = true;
 
-    @Override
-    public void process(int delta, World world) {
-	  currentJumpCooldown-=delta;
-	  if (currentJumpCooldown <= 0){
-		  jump = true;
-		  currentJumpCooldown = jumpCooldown;
-	  }
-        for (IEntity entity : world.getEntities()) {
-            if (entity instanceof Player) {
-                Boolean hitByPlatform = false;
-                ICharacter c = (ICharacter) entity;
-                if (world.getMoveEvents().isEmpty()) {
-                    entity.setVelocity(0, entity.getVelocity().getY());
-                }
-                for (Event k : entity.getEvents()) {
-                    if (k.getEvent() == HIT) {
-                        HitEvent hit = (HitEvent) k;
-                        if (hit.getSource() instanceof Platform) {
-                            Rectangle rectangle = (Rectangle) hit.getSource().getShape();
-                            Platform platform = (Platform) hit.getSource();
-                            Circle circle = (Circle) entity.getShape();
-                            if (entity.getPosition().getX() > platform.getPosition().getX() - rectangle.getWidth() / 2 && entity.getPosition().getX() < platform.getPosition().getX() + rectangle.getWidth() / 2 && entity.getPosition().getY() - circle.getRadius() < platform.getPosition().getY() + rectangle.getHeight() / 2) {
-                                entity.setPosition(entity.getPosition().getX(), (platform.getPosition().getY() - rectangle.getHeight() / 2) - circle.getRadius());
-                            }
-                            hitByPlatform = true;
-                            c.setFalling(false);
-                            entity.removeEvent(k);
-                        }
-                    }
-                }
-                // if hitCounter == 0(no hit with platforms), entity should fall.
-                if (hitByPlatform == false) {
-                    c.setFalling(true);
-                }
-                for (Event h : entity.getEvents()) {
-                    if (h.getEvent() == HIT) {
-                        HitEvent hit = (HitEvent) h;
-                        ICharacter character = (ICharacter) hit.getSource();
-                        if (character.getCharacterEnum() == ENEMY) {
-                            c.setHP(0);
-                        }
-                    }
-                }
-                if (c.getHP() <= 0) {
-                    entity.setIsDestroyed(true);
-                }
-                for (Event e : world.getMoveEvents()) {
-			if (e.getEvent() == W){
-				if (jump){
-				 	entity.setVelocity(entity.getVelocity().getX(), -15);
-					jump = false;
+	@Override
+	public void process(int delta, World world) {
+		currentJumpCooldown -= delta;
+		if (currentJumpCooldown <= 0) {
+			jump = true;
+		}
+		for (IEntity entity : world.getEntities()) {
+			if (entity instanceof Player) {
+				Boolean hitByPlatform = false;
+				ICharacter c = (ICharacter) entity;
+				if (world.getMoveEvents().isEmpty()) {
+					entity.setVelocity(0, entity.getVelocity().getY());
+				}
+				for (Event k : entity.getEvents()) {
+					if (k.getEvent() == HIT) {
+						HitEvent hit = (HitEvent) k;
+						if (hit.getSource() instanceof Platform) {
+							Rectangle rectangle = (Rectangle) hit.getSource().getShape();
+							Platform platform = (Platform) hit.getSource();
+							Circle circle = (Circle) entity.getShape();
+							if (entity.getPosition().getX() > platform.getPosition().getX() - rectangle.getWidth() / 2 && entity.getPosition().getX() < platform.getPosition().getX() + rectangle.getWidth() / 2 && entity.getPosition().getY() - circle.getRadius() < platform.getPosition().getY() + rectangle.getHeight() / 2) {
+								entity.setPosition(entity.getPosition().getX(), (platform.getPosition().getY() - rectangle.getHeight() / 2) - circle.getRadius());
+							}
+							hitByPlatform = true;
+							c.setFalling(false);
+							entity.removeEvent(k);
+						}
+					}
+				}
+				// if hitCounter == 0(no hit with platforms), entity should fall.
+				if (hitByPlatform == false) {
+					c.setFalling(true);
+				}
+				for (Event h : entity.getEvents()) {
+					if (h.getEvent() == HIT) {
+						HitEvent hit = (HitEvent) h;
+						ICharacter character = (ICharacter) hit.getSource();
+						if (character.getCharacterEnum() == ENEMY) {
+							c.setHP(0);
+						}
+					}
+				}
+				if (c.getHP() <= 0) {
+					entity.setIsDestroyed(true);
+				}
+				for (Event e : world.getMoveEvents()) {
+					if (e.getEvent() == W) {
+						if (jump) {
+							entity.setVelocity(entity.getVelocity().getX(), -15);
+							jump = false;
+							currentJumpCooldown = jumpCooldown;
+						}
+					}
+					if (e.getEvent() == S) {
+						//DUCK
+					}
+					if (e.getEvent() == D) {
+						entity.setVelocity(1, entity.getVelocity().getY());
+					}
+					if (e.getEvent() == A) {
+						entity.setVelocity(-1, entity.getVelocity().getY());
+					}
+					if (e.getEvent() == SPACE) {
+						Event event = new Event(SHOOT);
+						entity.addEvent(event);
+					}
+					if (e.getEvent() == CONTROL) {
+					}
 				}
 			}
-                    if (e.getEvent() == S) {
-                        //DUCK
-                    }
-                    if (e.getEvent() == D) {
-                        entity.setVelocity(1, entity.getVelocity().getY());
-                    }
-                    if (e.getEvent() == A) {
-                        entity.setVelocity(-1, entity.getVelocity().getY());
-                    }
-                    if (e.getEvent() == SPACE) {
-                        Event event = new Event(SHOOT);
-                        entity.addEvent(event);
-                    }
-                    if (e.getEvent() == CONTROL) {
-                    }
-                }
-            }
-        }
-    }
-
-    private void jump(int i){
-	    
-    }
+		}
+	}
 }
